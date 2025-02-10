197423a136412725f6fe0b72d95a2d387e6c7a23
tashimba <timur.tashimbetov@gmail.com>
2025-02-11 00:46:10 +0300

    init, header`s done

---
+++ components/ui/index.ts
@@ -0,0 +1,8 @@
+export { Button } from "./button";
+export { Input } from "./input";
+export { Dialog } from "./dialog";
+export { Select } from "./select";
+export { Slider } from "./slider";
+export { Drawer } from "./drawer";
+export { Checkbox } from "./checkbox";
+export { Popover } from "./popover";
